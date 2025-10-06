9ac223c4d1fa917dc4d5af1b63efc12f5efbe3ce
auliaa <auliaariski2507@gmail.com>
2025-10-06 16:47:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Efek animasi smooth scroll masuk halaman
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".fade-in");
+  sections.forEach((sec) => {
+    sec.style.opacity = 0;
+    setTimeout(() => {
+      sec.style.transition = "opacity 1s ease-out";
+      sec.style.opacity = 1;
+    }, 200);
+  });
+});
 
